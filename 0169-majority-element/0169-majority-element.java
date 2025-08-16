af6dbabb8c0b721dbeed828a088ebdb5fa31eac0
Rutuja musale <rutujamusale662@gmail.com>
2025-08-16 18:04:09 +0530

    Time: 1 ms (99.87%), Space: 53.4 MB (28.92%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int number=0;
+        for(int num :nums){
+            if(count == 0){
+                number = num;
+            }
+            if(num==number){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return number;
+    }
+}
